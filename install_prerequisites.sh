6071fb39641df90b6f6c8398159db9681f0b9fda
Yuri Bilenkis <iurii.bilenkis@deliveryhero.com>
2023-10-05 11:42:11 +0200

    fix install_prerequisites

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -4,18 +4,19 @@ set -xe
 PYTHON_VERSION="3.11.3"
 
 #xcode-select --install
-#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install terragrunt
 #brew install zsh pyenv pyenv-virtualenv
 #pyenv install $PYTHON_VERSION
-pyenv global $PYTHON_VERSION
+# pyenv global $PYTHON_VERSION
 
-curl -Lo /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-python3 /tmp/get-pip.py
-rm /tmp/get-pip.py
+# curl -Lo /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+# python3 /tmp/get-pip.py
+# rm /tmp/get-pip.py
 
-pyenv virtualenv $PYTHON_VERSION main
-pyenv activate main
-pip3 install --upgrade pip3
-pip3 install -r requirements.txt
-git submodule update
+# pyenv virtualenv $PYTHON_VERSION main
+# pyenv activate main
+# pip3 install --upgrade pip3
+# pip3 install -r requirements.txt
+# git submodule update
